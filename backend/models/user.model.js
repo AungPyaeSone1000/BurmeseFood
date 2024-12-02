66f428d287e5990a22e1e9680499a81fc6af6ff9
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-12-01 23:05:52 -0500

    password compare

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -50,4 +50,8 @@ userSchema.pre("save", async function (next) {
   }
 });
 
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
 export default User;
