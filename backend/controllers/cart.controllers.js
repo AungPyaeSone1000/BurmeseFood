3bc16e3e0ea6c7aa4f287329ec62963a157cf53e
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-12-09 16:28:31 -0500

    updateProductQuantity

--- backend/controllers/cart.controllers.js
+++ backend/controllers/cart.controllers.js
@@ -34,3 +34,27 @@ export const removeAllProducts = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const updateProductQuantity = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+    if (existingItem) {
+      if (quantity === 0) {
+        user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+        await user.save();
+        return res.json(user.cartItems);
+      }
+      existingItem.quantity = quantity;
+      await user.save();
+      res.json(user.cartItems);
+    } else {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+  } catch (error) {
+    console.log("Update product quantity error", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
