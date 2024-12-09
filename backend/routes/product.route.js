456d5ba8bd3b883cdf1cc18cb8ac9006480bf250
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-12-09 02:01:15 -0500

    Product Routes

---
+++ backend/routes/product.route.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  getProducts,
+  getFeaturedProducts,
+  createProduct,
+  deleteProduct,
+  getRecommendedProducts,
+  getProductsByCategory,
+  toggleFeaturedProduct
+} from "../controllers/product.controllers.js";
+import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, adminRoute, getProducts);
+router.get("/featured", getFeaturedProducts);
+router.get("/recommendation", getRecommendedProducts);
+router.get("category/:category", getProductsByCategory);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
+
+export default router;
