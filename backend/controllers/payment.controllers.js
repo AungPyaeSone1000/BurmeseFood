224a2f20f8d805ce8e8ed6a465991a7b8b97366b
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-12-10 01:36:29 -0500

    Payment controllers

---
+++ backend/controllers/payment.controllers.js
@@ -0,0 +1,133 @@
+import { stripe } from "../lib/stripe.js";
+import Coupon from "../models/coupon.model.js";
+import Order from "../models/order.model.js";
+
+export const createChceckoutSession = async (req, res) => {
+  try {
+    const { products, couponCode } = req.body;
+    if (!Array.isArray(products) || products.length === 0) {
+      return res.status(400).json({ message: "Invalid or empty products" });
+    }
+    let totalAmount = 0;
+    const lineItems = products.map((product) => {
+      const amount = Math.round(product.price * 100);
+      totalAmount += amount * product.quantity;
+
+      return {
+        price_data: {
+          currency: "cad",
+          product_data: {
+            name: product.name,
+            images: [product.image],
+          },
+          unit_amount: amount,
+        },
+      };
+    });
+
+    let coupon = null;
+    if (couponCode) {
+      coupon = await Coupon.findOne({
+        code: couponCode,
+        userId: req.user._id,
+        isActive: true,
+      });
+      if (coupon) {
+        totalAmount -= Math.round(
+          totalAmount * (coupon.discountPercentage / 100)
+        );
+      }
+    }
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${process.env.CLIENT_URL}/phurchase-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.CLIENT_URL}/phurchase-cancel`,
+      discounts: coupon
+        ? [{ coupon: await CreatestripeCoupon(coupon.discountPercentage) }]
+        : [],
+      metadata: {
+        userId: req.user._id.toString(),
+        couponCode: couponCode || "",
+        products: JSON.stringify(
+          products.map((product) => ({
+            id: product.id,
+            quantity: product.quantity,
+            price: product.price,
+          }))
+        ),
+      },
+    });
+    //200*100 cause stripe takes amount in cents
+    if (totalAmount >= 20000) {
+      await createNewCoupone(req.user._id);
+    }
+    res.status(200).json({ id: session.id, totalAmount: totalAmount / 100 });
+  } catch (error) {
+    console.log("Create checkout session error", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+async function CreatestripeCoupon(discountPercentage) {
+  const coupon = await stripe.coupons.create({
+    percent_off: discountPercentage,
+    duration: "once",
+  });
+  return coupon.id;
+}
+
+async function createNewCoupone(userId) {
+  const newCoupon = new Coupon({
+    code: "GIFT" + Math.random().toString(36).substring(2, 9).upperCase(),
+    discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), //30 days
+    isActive: true,
+    userId: userId,
+  });
+  await newCoupon.save();
+  return newCoupon;
+}
+
+export const checkoutSuccess = async (req, res) => {
+  try {
+    const { session_id } = req.body;
+    const session = await stripe.checkout.sessions.retrieve(session_id);
+    if (session.payment_status === "paid") {
+      if (session.metadata.couponCode) {
+        await Coupon.findOneAndUpdate(
+          {
+            code: session.metadata.couponCode,
+            userId: session.metadata.userId,
+          },
+          { isActive: false }
+        );
+      }
+      //Create new order
+      const products = JSON.parse(session.metadata.products);
+      const newOrder = new Order({
+        user: session.metadata.userId,
+        products: products.map((product) => ({
+          product: product.id,
+          quantity: product.quantity,
+          price: product.price,
+        })),
+        totalAmount: session.amount_total / 100,
+        stripeSessionId: session_id,
+      });
+
+      await newOrder.save();
+      res
+        .status(200)
+        .json({
+          success: true,
+          message: "Order created successfully",
+          orderId: newOrder._id,
+        });
+    }
+  } catch (error) {
+    console.log("Checkout success error", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
