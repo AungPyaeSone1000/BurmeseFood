750ff6af8a38d20e04c0ad2a848d6c25c7571cd0
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-12-16 08:25:26 -0500

    ProductList

---
+++ frontend/src/components/ProductsList.jsx
@@ -0,0 +1,104 @@
+import { motion } from "framer-motion";
+import { Trash, Star } from "lucide-react";
+import { useProductStore } from "../stores/useProductStore";
+
+const ProductsList = () => {
+  const { deleteProduct, toggleFeaturedProduct, products } = useProductStore();
+
+  return (
+    <motion.div
+      className="bg-white rounded-lg shadow-lg overflow-hidden max-w-4xl mx-auto mt-4"
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+    >
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-200">
+          <tr>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider"
+            >
+              Product Name
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider"
+            >
+              Price
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider"
+            >
+              Category
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider"
+            >
+              Featured
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider"
+            >
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-200 bg-white">
+          {products.map((product) => (
+            <tr key={product.id} className="bg-white hover:bg-gray-50">
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0 h-10 w-10">
+                    <img
+                      src={product.image}
+                      alt={product.name}
+                      className="h-10 w-10 rounded-full object-cover"
+                    />
+                  </div>
+                  <div className="ml-4">
+                    <div className="text-sm font-medium text-black">
+                      {product.name}
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="text-sm text-gray-900">
+                  {product.price.toFixed(2)}
+                </div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="text-sm text-gray-900">{product.category}</div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <button
+                  onClick={() => toggleFeaturedProduct(product.id)}
+                  className={`p-1 rounded-full ${
+                    product.featured
+                      ? "bg-yellow-400 text-white"
+                      : "bg-gray-300 text-gray-700"
+                  } hover:bg-yellow-500 transition-colors duration-200`}
+                >
+                  <Star className="w-5 h-5" />
+                </button>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                <button
+                  onClick={() => deleteProduct(product.id)}
+                  className="text-red-500 hover:text-red-300"
+                >
+                  <Trash className="w-5 h-5" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </motion.div>
+  );
+};
+export default ProductsList;
