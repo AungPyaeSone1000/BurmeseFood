bd2b6b9be1cde1d48d033f4b557ac3186fd4e59d
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-12-16 20:58:58 -0500

    Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,56 @@
-import { Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useUserStore } from "./stores/useUserStore";
+import { useEffect } from "react";
+
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LogInPage from "./pages/LogInPage.jsx";
+import AdminPage from "./pages/AdminPage.jsx";
+
+import Navbar from "./components/Navbar.jsx";
+import LoadingSpinner from "./components/LoadingSpinner.jsx";
 
 function App() {
+  const { user, checkAuth, checkingAuth } = useUserStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (checkingAuth) {
+    return <LoadingSpinner />;
+  }
+
   return (
-   <div>
-    <Route>
-      <Route path="/" element={<HomePage />} />
-    </Route>
-   </div>
+    <div className="min-h-screen  bg-green-100 text-gray-900 relative overflow-hidden">
+      {/* Background gradient */}
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(255,204,0,0.8)_0%,rgba(255,229,102,0.5)_45%,rgba(0,0,0,0.1)_100%)]" />
+        </div>
+      </div>
+      <div className="relative z-50 pt-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/signup"
+            element={!user ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!user ? <LogInPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/secret-dashboard"
+            element={
+              user?.role === "admin" ? <AdminPage /> : <Navigate to="/login" />
+            }
+          />
+        </Routes>
+      </div>
+      <Toaster />
+    </div>
   );
 }
 
