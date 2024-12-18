6ec05a0549cb51fd2fbcb6a987de61a847c4d300
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-12-18 07:04:26 -0500

    Route bug fixed

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { createChceckoutSession } from "../controllers/payment.controllers.js";
+import { createChceckoutSession, checkoutSuccess } from "../controllers/payment.controllers.js";
 
 const router = express.Router();
 
 router.post("/create-checkout-session", protectRoute, createChceckoutSession);
-router.post("/checkout-success", protectRoute, createChceckoutSession);
+router.post("/checkout-success", protectRoute, checkoutSuccess);
 
 
 export default router;
